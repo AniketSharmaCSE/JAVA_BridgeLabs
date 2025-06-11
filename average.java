20c4e7108548d41144676b0cbbf656e234f12be0
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-06-11 10:59:50 +0530

    [Aniket]Add. Second Practice question

---
+++ average.java
@@ -0,0 +1,9 @@
+public class average{
+	public static void main(String[] args){
+		int Maths=94,  Physics = 95, Chemistry = 96;
+		System.out.println("Sam’s average mark in PCM is "+ (Maths+Physics+Chemistry)/3);
+		
+		
+	}
+
+}
