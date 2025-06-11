cfd1607e47e06facc73876820f2e5fa89644f98e
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-06-11 16:25:45 +0530

    [Aniket]add. Thirteenth practice question

---
+++ sideOfSquare.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sideOfSquare{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double perimeter = sc.nextDouble();
+		double length = perimeter/4;
+		System.out.println("The length of the side is "+length+" whose perimeter is "+perimeter);
+	}
+}
