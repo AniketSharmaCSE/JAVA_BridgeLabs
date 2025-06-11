5d0b7339557547b27f4dfb4650eeb9fd23ab6e8b
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-06-11 11:18:33 +0530

    [Aniket]add third practice question

---
+++ kilometersToMiles.java
@@ -0,0 +1,9 @@
+public class kilometersToMiles{
+	public static void main(String[] args){
+		double Distance=10.8;
+		System.out.println("The distance 10.8km in miles is  "+ (10.8*1.6);
+		
+		
+	}
+
+}
