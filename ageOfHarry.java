3d14d8ae3e292d9b2c22ec6635c7f50d17f6d44f
Aniket Sharma <156219913+AniketSharmaCSE@users.noreply.github.com>
2025-06-12 09:23:33 +0530

    Delete ageOfHarry.java

--- ageOfHarry.java
+++
@@ -1,9 +0,0 @@
-public class ageOfHarry{
-	public static void main(String[] args){
-		int birth_year=2000,  current_year = 2024;
-		System.out.println("Harry's age in 2024 is "+ (current_year - birth_year));
-		
-		
-	}
-
-}
